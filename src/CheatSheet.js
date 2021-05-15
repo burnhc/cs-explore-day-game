fac77b8c044373ecb37327f8d3be3ca98ff0fbaa
Chandra <burnhc@uw.edu>
2021-05-15 01:19:22 -0700

    cleaned files

--- src/CheatSheet.js
+++ src/CheatSheet.js
@@ -7,11 +7,6 @@ export default function CheatSheet() {
         <div id={'CheatSheet'}>
             <Typography variant="h5">Cheat Sheet</Typography>
             <Typography variant="h6">Data Types</Typography>
-            <p>
-            Computers are pretty amazing; they can work with many types of data, like
-            strings (text), integers (numbers), and objects. For now, we'll only need
-            two of them: strings and integers.
-            </p>
             <Typography variant="button">Strings</Typography>
             <p>
             A <b>string</b> is a group of characters on your keyboard. Your computer recognizes
@@ -22,8 +17,8 @@ export default function CheatSheet() {
             </p>
             <Typography variant="button">Integers</Typography>
             <p>
-            An <b>integer</b> is just any whole number (without decimals). We don't need 
-            quotation marks for these. Here are some integers:
+            An <b>integer</b> is just any whole number (without decimals). Numbers are written
+            without quotation marks. Here are some integers:
             <br/><br/>
             <code>34</code><code>0</code><code>-14875</code>
             </p>
@@ -32,27 +27,17 @@ export default function CheatSheet() {
             <Typography variant="h6">Variables</Typography>
             <p>
             A <b>variable</b> is a tiny piece of computer memory that can store data,
-            like how variables can store numbers in math! We can create variables to
-            use in our code. Below, we create a variable by using <code>=</code> sign.
+            like how variables can store numbers in math! Below, we create a variable by using <code>=</code> sign.
             <br/><br/>
             <code>x = 5</code>
             <br/><br/>
-            The variable is called <code>x</code> and stores the integer 5. We can
-            name variables anything we want, but each variable name must be unique. 
-            <div className='blockText'>
-                BEWARE! In code, don't read <code>=</code> as "equals"; read it as
-                the word <b>"stores"</b>. Why? Because we also use <code>=</code>
-                to <i>change</i> a variable's value, which you'll see shortly.
-                <br/><br/>
-                For example, consider the statement <code>letters = "abc"</code>.
-                <br/>
-                This reads: the variable <code>letters</code> <b>stores</b> the string "abc".
-            </div>
+            The variable is called <code>x</code> and stores the integer 5. You can
+            name variables anything you want, but each variable name must be unique. 
             You can also change the value of variables in the same way:
             <br/><br/>
             <code>x = 6</code>
             <br/><br/>
-            Now, <code>x</code> no longer stores 5 -- it stores 6! We can also do this:
+            Now, <code>x</code> no longer stores 5 -- it stores 6! You can also do this:
             <br/><br/>
             <code>x = x + 5</code>
             <br/><br/>
